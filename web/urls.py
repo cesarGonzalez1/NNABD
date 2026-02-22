ec6e9ea58806de7bbf55cac4943dedc5015ed1e9
Emiliano Perez <perez.jimenez.emiliano.06@gmail.com>
2026-02-22 16:37:02 -0600

    consultar_editar

--- web/urls.py
+++ web/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.login_view, name='login'),
-    
+    path('empleado/<int:empleado_id>/', views.consultar_empleado, name='consultar_empleado'),
+    path('empleado/<int:empleado_id>/editar/', views.editar_empleado, name='editar_empleado'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
-def login_view(request):
-    return render(request, 'login.html')
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Empleado
+from .forms import EmpleadoForm
+
+def consultar_empleado(request, empleado_id):
+    empleado = get_object_or_404(Empleado, id=empleado_id)
+    return render(request, 'consultar_empleado.html', {'empleado': empleado})
+
+def editar_empleado(request, empleado_id):
+    empleado = get_object_or_404(Empleado, id=empleado_id)
+
+    if request.method == 'POST':
+        form = EmpleadoForm(request.POST, instance=empleado)
+        if form.is_valid():
+            form.save()
+            return redirect('consultar_empleado', empleado_id=empleado.id)
+    else:
+        form = EmpleadoForm(instance=empleado)
+
+    return render(request, 'editar_empleado.html', {'form': form})
 
 # Create your views here.
